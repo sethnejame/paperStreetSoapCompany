25eb432fdd5bf486f16ba3b50dfb457d0c84809b
Seth NeJame <sethnejame@gmail.com>
2019-11-10 17:46:24 +0100

    Adding first name and last name to Customer class

--- paperStreetSoapCompanyLib/Customer.cs
+++ paperStreetSoapCompanyLib/Customer.cs
@@ -2,7 +2,21 @@
 
 namespace paperStreetSoapCompanyLib
 {
-    public class Customer
-    {
-    }
+  public class Customer
+  {
+		public string FirstName { get; set; }
+    private string _lastName;
+		public string LastName
+		{
+			get
+			{
+				return _lastName;
+			}
+			set
+			{
+				_lastName = value;
+			}
+		}
+
+  }
 }
