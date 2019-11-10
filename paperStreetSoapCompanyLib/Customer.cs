956207e58c1c8310523acdec612a0621f63ec03b
Seth NeJame <sethnejame@gmail.com>
2019-11-10 17:51:18 +0100

    Adding email, custID to cust class, fullname

--- paperStreetSoapCompanyLib/Customer.cs
+++ paperStreetSoapCompanyLib/Customer.cs
@@ -4,6 +4,8 @@ namespace paperStreetSoapCompanyLib
 {
   public class Customer
   {
+		public int CustomerId { get; private set; }
+		public string Email { get; set; }
 		public string FirstName { get; set; }
     private string _lastName;
 		public string LastName
@@ -15,6 +17,13 @@ namespace paperStreetSoapCompanyLib
 			set
 			{
 				_lastName = value;
+			}
+		}
+		public string FullName
+		{
+			get
+			{
+				return LastName + "," + FirstName;
 			}
 		}
 
