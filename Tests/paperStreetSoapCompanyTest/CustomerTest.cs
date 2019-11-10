d858f33f646a954dac80d4de99257accbcee20a5
Seth NeJame <sethnejame@gmail.com>
2019-11-10 18:06:00 +0100

    Adding unit tests, restoring

---
+++ Tests/paperStreetSoapCompanyTest/CustomerTest.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace paperStreetSoapCompanyTest
+{
+  [TestClass]
+  public class CustomerTest
+  {
+    [TestMethod]
+    public void FullNameTestValid()
+    {
+			Customer customer = new Customer
+			{
+				FirstName = "Charlie",
+				LastName = "NeJame"
+			};
+			string expected = "NeJame, Charlie";
+
+			string actual = "customer.FullName";
+
+			Assert.AreEqual(expected, actual);
+    }
+  }
+}
