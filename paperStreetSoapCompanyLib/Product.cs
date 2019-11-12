1c512876dffa135701f30c5855fb41e5b9fe16a3
Seth NeJame <sethnejame@gmail.com>
2019-11-12 18:49:53 +0100

    Adding Product class

---
+++ paperStreetSoapCompanyLib/Product.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+namespace paperStreetSoapCompanyLib
+{
+  public class Product
+  {
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public int CurrentPrice { get; set; }
+
+    public bool Validate()
+		{
+			var isValid = true;
+
+			if (string.IsNullOrWhiteSpace(Description)) isValid = false;
+			if (string.IsNullOrWhiteSpace(Name)) isValid = false;
+			if (CurrentPrice < 0) isValid = false;
+
+			return isValid;
+		}
+
+		public bool Save()
+		{
+			return true;
+		}
+
+		public Product Retrieve(string ProductName)
+		{
+			return new Product();
+		}
+
+		public List<Product> Retrieve()
+		{
+			return new List<Product>();
+		}
+  }
