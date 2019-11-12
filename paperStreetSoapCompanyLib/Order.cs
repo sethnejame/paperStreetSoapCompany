382ef3af05b4d61182091f541ad5ebf930e33814
Seth NeJame <sethnejame@gmail.com>
2019-11-12 19:09:47 +0100

    Rollback, adding separate order class:

---
+++ paperStreetSoapCompanyLib/Order.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+namespace paperStreetSoapCompanyLib
+{
+  public class Order
+  {
+    public Order()
+		{
+
+		}
+    public Order(int orderId)
+		{
+			OrderId = orderId;
+		}
+    public DateTimeOffset? OrderDate { get; set; }
+
+    public bool Validate()
+		{
+			var isValid = true;
+
+			if (OrderDate == null) isValid = false;
+
+			return isValid;
+		}
+
+		public bool Save()
+		{
+			return true;
+		}
+
+		public Order Retrieve(int orderId)
+		{
+			return new Order();
+		}
+
+		public List<OrderItem> Retrieve()
+		{
+			return new List<OrderItem>();
+		}
+  }
