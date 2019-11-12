fd20c5c48344148894828f7004e2a9dc112b01f7
Seth NeJame <sethnejame@gmail.com>
2019-11-12 18:59:14 +0100

    Adding OrderItem class:

---
+++ paperStreetSoapCompanyLib/OrderItem.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+
+namespace paperStreetSoapCompanyLib
+{
+  public class OrderItem
+  {
+    public OrderItem()
+		{
+
+		}
+    public OrderItem(int orderItemId)
+		{
+			OrderItemId = orderItemId;
+		}
+    public string Product { get; set; }
+    public int OrderItemId { get; private set; }
+
+    public int Quantity { get; set; }
+    public int PurchasePrice { get; set; }
+
+    public bool Validate()
+		{
+			var isValid = true;
+
+			if (string.IsNullOrWhiteSpace(Product)) isValid = false;
+			if (Quantity < 0) isValid = false;
+			if (PurchasePrice < 0) isValid = false;
+
+
+			return isValid;
+		}
+
+		public bool Save()
+		{
+			return true;
+		}
+
+		public OrderItem Retrieve(int orderItemId)
+		{
+			return new OrderItem();
+		}
+
+		public List<OrderItem> Retrieve()
+		{
+			return new List<OrderItem>();
+		}
+  }
