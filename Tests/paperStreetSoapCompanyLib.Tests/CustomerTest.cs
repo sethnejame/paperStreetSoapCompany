62ad0e37331ce096db157249fed159c7ef9b7050
Seth NeJame <sethnejame@gmail.com>
2019-11-12 13:00:03 +0100

    Adding Instance Count static member to Customer for count

--- Tests/paperStreetSoapCompanyLib.Tests/CustomerTest.cs
+++ Tests/paperStreetSoapCompanyLib.Tests/CustomerTest.cs
@@ -8,42 +8,60 @@ namespace paperStreetSoapCompanyLib.Tests
     [TestMethod]
     public void FullNameTestValid()
     {
-			Customer customer = new Customer
-			{
-				FirstName = "Charlie",
-				LastName = "NeJame"
-			};
-			string expected = "NeJame, Charlie";
+      Customer customer = new Customer
+      {
+        FirstName = "Charlie",
+        LastName = "NeJame"
+      };
+      string expected = "NeJame, Charlie";
 
-			string actual = customer.FullName;
+      string actual = customer.FullName;
 
-			Assert.AreEqual(expected, actual);
+      Assert.AreEqual(expected, actual);
     }
     [TestMethod]
     public void FullNameFirstNameEmpty()
     {
-			Customer customer = new Customer
-			{
-				LastName = "NeJame"
-			};
-			string expected = "NeJame";
+      Customer customer = new Customer
+      {
+        LastName = "NeJame"
+      };
+      string expected = "NeJame";
 
-			string actual = customer.FullName;
+      string actual = customer.FullName;
 
-			Assert.AreEqual(expected, actual);
+      Assert.AreEqual(expected, actual);
     }
     [TestMethod]
     public void FullNameLastNameEmpty()
     {
-			Customer customer = new Customer
-			{
-				FirstName = "Charlie"
-			};
-			string expected = "Charlie";
+      Customer customer = new Customer
+      {
+        FirstName = "Charlie"
+      };
+      string expected = "Charlie";
 
-			string actual = customer.FullName;
+      string actual = customer.FullName;
 
-			Assert.AreEqual(expected, actual);
+      Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void StaticTest()
+    {
+      var c1 = new Customer();
+      c1.FirstName = "Charlie";
+      Customer.InstanceCount += 1;
+
+      var c2 = new Customer();
+      c2.FirstName = "Milo";
+      Customer.InstanceCount += 1;
+
+      var c3 = new Customer();
+      c3.FirstName = "Fat Bob";
+      Customer.InstanceCount += 1;
+
+      Assert.AreEqual(3, Customer.InstanceCount);
     }
   }
 }
