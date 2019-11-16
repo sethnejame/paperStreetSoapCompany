4d447c4c069e928bb2087f103243a6f49adcb781
Seth NeJame <sethnejame@gmail.com>
2019-11-16 11:13:53 +0100

    Adding cust type to cust class (inheriting classes to follow)

--- paperStreetSoapCompanyLib/Order.cs
+++ paperStreetSoapCompanyLib/Order.cs
@@ -5,24 +5,29 @@ namespace paperStreetSoapCompanyLib
 {
   public class Order
   {
-    public Order()
-		{
+    public Order() : this(0)
+    {
 
-		}
+    }
     public Order(int orderId)
-		{
-			OrderId = orderId;
-		}
+    {
+      OrderId = orderId;
+      OrderItems = new List<OrderItem>();
+    }
     public int OrderId { get; private set; }
     public DateTimeOffset? OrderDate { get; set; }
+    public int CustomerId { get; set; }
+    public int ShippingAddressId { get; set; }
+    public List<OrderItem> OrderItems { get; set; }
+    public int CustomerType { get; set; }
 
     public bool Validate()
-		{
-			var isValid = true;
+    {
+      var isValid = true;
 
-			if (OrderDate == null) isValid = false;
+      if (OrderDate == null) isValid = false;
 
-			return isValid;
-		}
+      return isValid;
+    }
   }
 }
