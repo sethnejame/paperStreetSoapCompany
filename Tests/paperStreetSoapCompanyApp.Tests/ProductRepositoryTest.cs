33d6eb119688e3d5517417b5b321c8c45ccb307a
Seth NeJame <sethnejame@gmail.com>
2019-11-20 07:28:22 +0100

    Adding Save method to product repo, adding unit testing

--- Tests/paperStreetSoapCompanyApp.Tests/ProductRepositoryTest.cs
+++ Tests/paperStreetSoapCompanyApp.Tests/ProductRepositoryTest.cs
@@ -22,5 +22,21 @@ namespace paperStreetSoapCompanyApp.Tests
       Assert.AreEqual(expected.ProductDescription, actual.ProductDescription);
       Assert.AreEqual(expected.CurrentPrice, actual.CurrentPrice);
     }
+     [TestMethod]
+    public void SaveTestValid()
+    {
+      var productRepo = new ProductRepository();
+      var updatedProduct = new Product(7)
+      {
+        ProductName = "Fancy Soap",
+        ProductDescription = "Made from the finest ingredients available",
+        CurrentPrice = 99,
+        HasChanges = true
+      };
+
+      var actual = productRepo.Save(updatedProduct);
+
+      Assert.AreEqual(true, actual);
+    }
   }
 }
