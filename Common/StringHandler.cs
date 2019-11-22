59d7536e14169c8b60539874ce28db4039faa9dc
Seth NeJame <sethnejame@gmail.com>
2019-11-22 06:42:08 +0100

    Adding common library, adding StringHandler method to common

---
+++ Common/StringHandler.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+namespace Common
+{
+  public class StringHandler
+  {
+    public string InsertSpaces(string source)
+    {
+      string result = string.Empty;
+
+      if (!String.IsNullOrWhiteSpace(source))
+      {
+        foreach (char letter in source)
+        {
+          if (char.IsUpper(letter))
+          {
+            result += " ";
+          }
+          result += letter;
+        }
+      }
+      return result;
+    }
+  }
+}
--- paperStreetSoapCompanyApp/Product.cs
+++ paperStreetSoapCompanyApp/Product.cs
@@ -13,7 +13,18 @@ namespace paperStreetSoapCompanyApp
 		{
 			ProductId = productId;
 		}
-    public string ProductName { get; set; }
+		private string _productName;
+    public string ProductName
+		{
+			get
+			{
+				return _productName;
+			}
+			set
+			{
+				_productName = value;
+			}
+		}
     public string ProductDescription { get; set; }
 
     public decimal? CurrentPrice { get; set; }
